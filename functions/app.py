0590e603ff2707bb90956976a017fd33ad8bb2d1
ot-nemoto <nemoto@opentone.co.jp>
2019-10-23 08:56:23 +0000

    change integration type aws to aws_proxy

--- functions/app.py
+++ functions/app.py
@@ -5,23 +5,38 @@ import json
 def lambda_handler(event, context):
   print(json.dumps(event))
 
-  expect_code = event.get('params').get('expect_code', '200')
-  if expect_code == '200':
-    return {
-      "statusCode": 200,
-      "body": {
-        "message": "hello world"
+  queryStringParameters = event.get('queryStringParameters') \
+    if event.get('queryStringParameters') is not None else {}
+  expect_code = queryStringParameters.get('expect_code', '200')
+  try:
+    if expect_code == '200':
+      return {
+        "statusCode": expect_code,
+        "body": json.dumps({
+          "message": "hello world"
+        })
       }
-    }
-  elif expect_code == '400':
-    raise Exception("Bad Request")
-  elif expect_code == '401':
+    elif expect_code == '400':
+      raise Exception("Bad Request")
+    elif expect_code == '401':
+      return {
+        "statusCode": 200,
+        "body": json.dumps({
+          "stackTrace": [],
+          "errorType": "Exception",
+          "errorMessage": "Unauthorixed"
+        })
+      }
+    elif expect_code == '403':
+      raise Exception("あなたにはアクセス権がありません")
+    else:
+      expect_code = 500
+      raise Exception("Internal Server Error")
+  except Exception as e:
+    print(e.message)
     return {
-      "stackTrace": [],
-      "errorType": "Exception",
-      "errorMessage": "Unauthorixed"
+      "statusCode": expect_code,
+      "body": json.dumps({
+        "message": e.message
+      })
     }
-  elif expect_code == '403':
-    raise Exception("あなたにはアクセス権がありません")
-  else:
-    raise Exception("Internal Server Error")
